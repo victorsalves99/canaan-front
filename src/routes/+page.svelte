065addb8f2d299d72c47d5361a8a243fe99ddc4a
victorsalves99 <victorsalves99@outlook.com>
2024-11-14 19:35:04 -0300

    criado a pagina de login e adicionado a logica

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { goto } from '$app/navigation';
+	import Button from '$lib/components/forms/Button.svelte';
+	import Password from '$lib/components/forms/Password.svelte';
+	import Text from '$lib/components/forms/Text.svelte';
+	import { toast } from '$lib/store/toastStore';
+	let usuario = '';
+	let senha = '';
+	function fazer_login() {
+		if (usuario == 'admin' && senha == 'admin') {
+			goto('/canaan');
+		} else {
+			toast('ERROR', true, 'Usuario ou senha invalidos!');
+		}
+	}
+</script>
+
+<section class="login">
+	<div class="box_login">
+		<h1>Faça Login</h1>
+		<Text label="Usuario" bind:valor={usuario} placeholder="EX: VictorAlves" />
+		<Password label="Senha" bind:valor={senha} />
+		<Button label="Login" funcaoClick={fazer_login} />
+	</div>
+</section>
+
+<style>
+	.login {
+		max-width: 100vw;
+		min-width: 100vw;
+		max-height: 100vh;
+		min-height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.box_login {
+		width: 500px;
+		padding: 50px;
+	}
+	h1 {
+		font-size: 1.4rem;
+		font-weight: bold;
+		margin-bottom: 2rem;
+	}
+</style>
